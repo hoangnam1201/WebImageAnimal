e0332212cb7bd10597e1915b14322ead7bf67174
hoangnam1201 <55224036+hoangnam1201@users.noreply.github.com>
2022-05-10 23:15:17 +0700

    Logout handler

--- fe_animal/src/Components/Header/index.js
+++ fe_animal/src/Components/Header/index.js
@@ -1,8 +1,16 @@
 import { Button } from "@mui/material";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../Assets/logo.png";
+import { useCookies } from "react-cookie";
 const Header = () => {
+  const [cookies, _, removeCookies] = useCookies(["infoUser"]);
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    removeCookies("infoUser", { path: "/" });
+    navigate("/");
+  };
   return (
     <nav className="flex justify-between  items-center font-400 z-10 text-gray-300 relative">
       <Link to="/" className="ml-5 pl-10">
@@ -14,31 +22,46 @@ const Header = () => {
         <li className="mx-3">Bussiness ideas</li>
         <li className="mx-3">Resources</li>
       </ul>
-      <div className="px-4 mr-10">
-        <Link to="/auth/register">
+      {cookies.infoUser ? (
+        <div className="flex px-4 mr-10">
+          <h2 className="font-bold text-xl text-white">
+            Hi, {cookies.infoUser.user.username}
+          </h2>
           <Button
+            onClick={logoutHandler}
+            className="ml-3 border-white text-white"
             variant="outlined"
-            style={{
-              color: "white",
-              borderColor: "white",
-              marginRight: "10px",
-              borderRadius: "10px",
-              fontWeight: "bold",
-            }}
           >
-            Become a contributor
+            Đăng xuất
           </Button>
-        </Link>
-        <Link to="/auth/login">
-          <Button
-            variant="contained"
-            style={{ borderRadius: "10px", fontWeight: "bold" }}
-            color="success"
-          >
-            Login
-          </Button>
-        </Link>
-      </div>
+        </div>
+      ) : (
+        <div className="px-4 mr-10">
+          <Link to="/auth/register">
+            <Button
+              variant="outlined"
+              style={{
+                color: "white",
+                borderColor: "white",
+                marginRight: "10px",
+                borderRadius: "10px",
+                fontWeight: "bold",
+              }}
+            >
+              Become a contributor
+            </Button>
+          </Link>
+          <Link to="/auth/login">
+            <Button
+              variant="contained"
+              style={{ borderRadius: "10px", fontWeight: "bold" }}
+              color="success"
+            >
+              Login
+            </Button>
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
